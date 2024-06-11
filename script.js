8b17248cd9fca5001e16894c3db3bdfb8916c1fb
Aditya Gupta <rajaditya2399@gmail.com>
2024-06-11 21:14:15 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+// let n = 5;
+// for(let i=0; i<n; i++){
+//     console.log("hello, ",i);
+// }
+
+
+
+//process: this object provides info about and control overpp the current node.js process.
+// process.argv: return an array containing the cli arguments passed when the nodejs process was launched
+
+//module.export a special object
+//require =  a built-in-function to include external module that exist is separate files
+const math = require("./math");
+console.log(math.sum(5,5))
+console.log(math.sub(5,5))
+console.log(math.mul(5,5))
+console.log(math.PI)
+console.log(math.g)
