7f39c7870a861642e9623e73251ed5da882450ab
Aditya Gupta <rajaditya2399@gmail.com>
2024-06-18 03:57:16 +0530

    upto update

--- Rest/index.js
+++ Rest/index.js
@@ -24,7 +24,10 @@ const port = 3000;
 
 const path = require("path");
 const { v4:uuidv4 } = require('uuid');
+const methodOverride = require('method-override')
+
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'))
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -71,13 +74,22 @@ app.get("/posts/:id",(req,res) =>{
     res.render("show.ejs",{ post });
 });
 
-app.patch("/post/:id",(req,res)=>{
-
+app.patch("/posts/:id", (req, res) => {
     let { id } = req.params;
-    console.log(id);
-    res.send("patch request working")
+    let newContent = req.body.content;
+    let post = posts.find((p) => id === p.id);
+    post.content = newContent;
+    res.redirect("/posts");
 });
 
+app.get("/posts/:id/edit",(req,res)=>{
+    let { id } = req.params;
+    let post = posts.find((p)=> id ===p.id);
+    res.render("edit.ejs",{post})
+
+
+
+});
 app.listen(port, () => {
     console.log(`listening on port ${port}`);
 });
