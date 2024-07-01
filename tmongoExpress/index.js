eb28f1f7005a3c7c234d8dfdbf64b58db6870d72
Aditya Gupta <rajaditya2399@gmail.com>
2024-07-02 04:25:04 +0530

    completed CRUD

--- tmongoExpress/index.js
+++ tmongoExpress/index.js
@@ -3,13 +3,15 @@ const app = express();
 const port = 3000;
 const mongoose = require("mongoose");
 const path = require("path");
+const methodOverride = require('method-override');
 
-const Chat = require("./models/chat.js")
 
+const Chat = require("./models/chat.js");
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-app.use(express.static(path.join(__dirname,"public")))
-app.use(express.urlencoded({extended:true}))
+app.use(express.static(path.join(__dirname,"public")));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
 
 
 main().then(() => { console.log("successful connection") })
@@ -36,6 +38,7 @@ app.get("/", (req, res) => {
 //index route
 app.get("/chats", async (req, res) => {
     let chats = await Chat.find()
+   
     res.render("index.ejs", { chats });
 });
 
@@ -61,6 +64,34 @@ app.post("/chats",(req,res) =>{
     });
     res.redirect("/chats");
 });
+
+//edit route
+
+app.get("/chats/:id/edit", async(req,res) =>{
+    let {id} = req.params
+    let chat = await Chat.findById(id);
+    res.render("edit.ejs" , {chat});
+    
+});
+
+//update route
+app.put("/chats/:id", async (req,res)=>{
+    let {id} = req.params;
+
+    let { msg : newMsg} = req.body;
+    console.log(newMsg);
+    let updatedChat = await Chat.findByIdAndUpdate(id, { msg: newMsg }, { runValidators: true, new: true });
+  
+    res.redirect("/chats");
+});
+
+//Destroy route
+app.delete("/chats/:id", async (req,res)=>{
+    let {id} = req.params;
+    console.log(req.body);
+    res.redirect("/chats")
+
+})
 app.listen(port, () => {
     console.log(`server is listening on ${port}`)
 });
